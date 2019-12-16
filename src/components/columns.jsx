e0fcefee84b00bfb3e263991c3a6137f5f8ade18
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-12-15 23:23:15 -0700

    able to add game pieces

---
+++ src/components/columns.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import gameBoard from '../logic'
+
+class Columns extends React.Component {
+    state = 
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 0, 0, 0]
+        ]
+
+        
+    render() {
+
+        const addPiece = (column, player) => {
+            if(this.state[this.state.length - 1][column] === 0) {
+                this.state[this.state.length - 1][column] = player
+            } else {
+                for(let row = 1; row < this.state.length; row++){
+                    if(this.state[row][column] > 0) {
+                        this.state[row - 1][column] = player
+                        this.forceUpdate()
+                        console.log(row - 1, column, 'return')
+                        return
+                    }
+                    console.log(row, column)
+                }
+            }
+            console.log(this.state.length)
+            this.forceUpdate()
+        }
+
+        return (
+            <div>
+                <button onClick={() => addPiece(0, 1)}>butt 1</button>
+                <button onClick={() => addPiece(1, 1)}>butt 2</button>
+                <button onClick={() => addPiece(2, 1)}>butt 3</button>
+                <button onClick={() => addPiece(3, 1)}>butt 4</button>
+                {this.state.map(board => <h1>{board}</h1>)}
+                <button onClick={() => addPiece(0, 2)}>butt 1</button>
+                <button onClick={() => addPiece(1, 2)}>butt 2</button>
+                <button onClick={() => addPiece(2, 2)}>butt 3</button>
+                <button onClick={() => addPiece(3, 2)}>butt 4</button>
+            </div>
+        )
+    }
+}
+
+export default Columns
---
+++ src/logic.js
@@ -0,0 +1,9 @@
+export let gameBoard = 
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+export default gameBoard
