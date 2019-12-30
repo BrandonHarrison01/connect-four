c45ff7e5597228758c5ee5d479f1243dfc5d27e7
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-12-29 22:05:07 -0700

    able to toggle between players

--- src/components/columns.jsx
+++ src/components/columns.jsx
@@ -3,27 +3,34 @@ import React from 'react'
 import '../columns.scss'
 
 class Columns extends React.Component {
-    state = 
-        [
+    state = {
+        board: [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 0]
-        ]
+        ],
+        player: 1
+    }
 
         
     render() {
 
         const addPiece = (column, player) => {
-            if(this.state[this.state.length - 1][column] === 0) {
-                this.state[this.state.length - 1][column] = player
-            } else if(this.state[0][column] > 0){
+            if(this.state.board[this.state.board.length - 1][column] === 0) {
+                this.state.board[this.state.board.length - 1][column] = player
+            } else if(this.state.board[0][column] > 0){
                 console.log('cannot move there')
                 return
             } else {
-                for(let row = 1; row < this.state.length; row++){
-                    if(this.state[row][column] > 0) {
-                        this.state[row - 1][column] = player
+                for(let row = 1; row < this.state.board.length; row++){
+                    if(this.state.board[row][column] > 0) {
+                        this.state.board[row - 1][column] = player
+                        if (player === 1){
+                            this.state.player = 2
+                        } else if(player === 2) {
+                            this.state.player = 1
+                        }
                         this.forceUpdate()
                         console.log(row - 1, column, 'return')
                         return
@@ -31,27 +38,32 @@ class Columns extends React.Component {
                     console.log(row, column)
                 }
             }
-            console.log(this.state.length)
+            if (player === 1){
+                this.state.player = 2
+            } else if(player === 2) {
+                this.state.player = 1
+            }
+            console.log(this.state.board.length)
             this.forceUpdate()
         }
 
         return (
             <div>
-                <div className="display-none">
+                <div className={this.state.player === 2 ? "display-none" : ""}>
                     <h2>Player 1</h2>
                     <button onClick={() => addPiece(0, 1)}>butt 1</button>
                     <button onClick={() => addPiece(1, 1)}>butt 2</button>
                     <button onClick={() => addPiece(2, 1)}>butt 3</button>
                     <button onClick={() => addPiece(3, 1)}>butt 4</button>
                 </div>
-                <div className="player-2">
-                    <h2>Player 1</h2>
+                <div className={this.state.player === 1 ? "display-none" : ""}>
+                    <h2>Player 2</h2>
                     <button onClick={() => addPiece(0, 2)}>butt 1</button>
                     <button onClick={() => addPiece(1, 2)}>butt 2</button>
                     <button onClick={() => addPiece(2, 2)}>butt 3</button>
                     <button onClick={() => addPiece(3, 2)}>butt 4</button>
                 </div>
-                {this.state.map(board => <h1>{board}</h1>)}
+                {this.state.board.map(board => <h1>{board}</h1>)}
             </div>
         )
     }
