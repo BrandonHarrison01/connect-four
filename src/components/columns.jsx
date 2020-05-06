88efe9dfbe7dd7298bb404aaeb19b81e0fbd480f
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-05-06 16:40:44 -0600

    working on adding a value to game board dynamically

--- src/components/columns.jsx
+++ src/components/columns.jsx
@@ -2,21 +2,41 @@ import React, { useState, useEffect } from 'react'
 import '../columns.scss'
 
 function Columns() {
+    let size = 5
     let boardSize = [[0]]
-    let [gameBoard, setGameBoard] = useState(boardSize)
 
-    for(let i = 1; i < 4; i++){
-       boardSize[0].push(0)
-       boardSize.push(boardSize[0])
+    //rerendering ↓ after every state change? setting value on every array to the same location of game piece?
+    //useEffect hook?
+    //... I don't think thats it, not causing a rerender
+    for(let i = 1; i < size; i++){
+        boardSize[0].push(0)
+        boardSize.push(boardSize[0])
     }
+
+    boardSize[2][4] = 'shite'
+    console.log(boardSize, 'test')
     
-    console.log(gameBoard)
+    const addPiece = index => {
+        for(let i = size - 1; i >= 0; i--){
+            if(boardSize[i][index] === 0){
+                boardSize[4][index] = '💩'
+                break
+            }
+        }
+        console.log(boardSize)
+        return
+    }
+
+    console.log('rerender')
 
     return(
-        <div>
-            {gameBoard.map(row => 
+        <div className='board'>
+            <div className='game-controls'>
+                {boardSize.map((val, index) => <p className='display-hidden' onClick={() => addPiece(index)} />)}
+            </div>
+            {boardSize[0].map(row => 
                 <div className='row'>
-                    {gameBoard[0].map(piece => <p className='empty'>he</p>)}
+                    {boardSize.map(piece => <p className='empty'/>)}
                 </div>)}
         </div>
     )
