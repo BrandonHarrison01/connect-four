824749254741660a180d10c1a63f9b191db532fd
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-08-03 16:59:24 -0600

    menu buttons are built

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Menu() {
+function Menu(props) {
   return (
     <div className='menu'>
       <h2>Main Menu</h2>
       <div className='menu-option'>
         <h3>Local</h3>
         <ul>
-          <li>
-            <Link to='/local-pvp'>PVP</Link>
-          </li>
-          <li>
-            <Link to='/local-ai'>Bot</Link>
-          </li>
+          <button onClick={() => props.history.push('/local-pvp')}>
+            PVP
+          </button>
+          <button onClick={() => props.history.push('/local-ai')}>
+            Bot
+          </button>
         </ul>
       </div>
       <div className='menu-option'>
         <h3>Online</h3>
+        <button className='coming'>Coming Soon</button>
       </div>
     </div>
   );
