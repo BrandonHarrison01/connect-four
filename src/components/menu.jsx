44581d8187520a83201fe4b239bece9589a267c2
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-09-23 16:56:53 -0600

    revisions of names and styles

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,21 +3,14 @@ import React from "react";
 function Menu(props) {
   return (
     <div className='menu'>
-      <h2>Main Menu</h2>
       <div className='menu-option'>
-        <h3>Local</h3>
-        <ul>
-          <button onClick={() => props.history.push('/local-pvp')}>
-            PVP
-          </button>
-          <button onClick={() => props.history.push('/local-ai')}>
-            Bot
-          </button>
-        </ul>
-      </div>
-      <div className='menu-option'>
-        <h3>Online</h3>
-        <p className='coming'>Coming Soon</p>
+        <h2>Main Menu</h2>
+        <button onClick={() => props.history.push('/local-pvp')}>
+          PVP
+        </button>
+        <button onClick={() => props.history.push('/local-ai')}>
+          Bot
+        </button>
       </div>
     </div>
   );
--- src/components/player-won.jsx
+++ src/components/player-won.jsx
@@ -6,7 +6,7 @@ function PlayerWon(props) {
             <div className='win-modal'>
                 {props.playerWon === 1 && <h3>black wins</h3>}
                 {props.playerWon === 2 && <h3>red wins</h3>}
-                <button onClick={props.resetBoard}>Reset</button>
+                <button onClick={props.resetBoard}>Reset Board</button>
                 <button onClick={() => props.history.push('/')}>Menu</button>
             </div>
         </div>
